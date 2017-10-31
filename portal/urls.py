6faa7f46080219da69da215eae4ff9932a445b8b
Prafful Kumar <kumarprafful.54@gmail.com>
2017-10-31 22:37:06 +0530

    added form for uploading

--- portal/urls.py
+++ portal/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import uploadView 
+from .views import uploadView, uploadFormView 
 urlpatterns = [
-	url(r'^$', uploadView ,name='upload_view')
+	url(r'^$', uploadView ,name='upload_view'),
+	url(r'^upload/$', uploadFormView ,name='upload_form_view')
 
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from .models import Upload
+from .forms import UploadForm
 
 # Create your views here.
 
 def uploadView(request):
 	files = Upload.objects.all()
 	return render(request, 'portal/index.html', {'files':files})
+
+def uploadFormView(request):
+	if request.method == 'POST':
+		upload_form = UploadForm(data=request.POST)
+
+		if upload_form.is_valid():
+			uploadForm = upload_form.save()
+
+			return HttpResponseRedirect(reverse('uploadView'))
+
+	else:
+		upload_form = UploadForm()
+	return render(request, 'portal/upload.html', {'upload_form': upload_form})
