5b1079855b7ae6a66d090bba9990f333adf6e0f0
Vishal Pal <pal.Vishal41@gmail.com>
2017-10-31 21:13:19 +0530

    Added downloadView url

--- portal/urls.py
+++ portal/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import uploadView 
+from .views import uploadView, downloadView
 urlpatterns = [
-	url(r'^$', uploadView ,name='upload_view')
+	url(r'^$', uploadView ,name='upload_view'),
+	url(r'^download_list/$', downloadView, name='download_view')
 
 ]
