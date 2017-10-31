8ccf813360d0fec0979c33d432c6bac6e9fe498a
Vishal Pal <pal.Vishal41@gmail.com>
2017-10-31 21:09:26 +0530

    Registered Download model

--- portal/admin.py
+++ portal/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Upload
+from .models import Upload, Download
 # Register your models here.
 
 admin.site.register(Upload, list_display=['title', 'dates'])
+admin.site.register(Download)
