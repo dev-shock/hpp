26ff56630ab5c2e2a984535107e50b2ecb8ff89c
Vishal Pal <pal.Vishal41@gmail.com>
2017-10-31 21:06:57 +0530

    Added Download_View

--- portal/views.py
+++ portal/views.py
@@ -6,3 +6,7 @@ from .models import Upload
 def uploadView(self):
 	files = Upload.objects.all()
 	return render(request, 'portal/index.html', {'files':files})
+
+def downloadView(self):
+	files = Download.objects.all()
+	return render(request, 'portal/download.html', {'files':files})
