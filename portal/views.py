6e9da17cca2ededdc0a57fb26f310727e7e270d6
Prafful Kumar <kumarprafful.54@gmail.com>
2017-10-31 22:01:29 +0530

    templates added

--- portal/views.py
+++ portal/views.py
@@ -3,6 +3,6 @@ from .models import Upload
 
 # Create your views here.
 
-def uploadView(self):
+def uploadView(request):
 	files = Upload.objects.all()
 	return render(request, 'portal/index.html', {'files':files})
